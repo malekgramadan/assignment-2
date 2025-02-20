846f3fe0d474c6997192184b3feea08800cd1414
Malek <mintywicked@gmail.com>
2025-02-20 10:05:52 +0200

    Adding the savings, rent and electricity percentages for the user to add

--- Nabiha's Salary.py
+++ Nabiha's Salary.py
@@ -4,4 +4,7 @@ list = []
 while action != "no":
     salary = float(input("Enter your salary: "))
     month = input("Enter the month: ")
+    savings_percent = float(input("Enter the percentage of salary you want to save: "))
+    rent_percent = float(input("Enter the percentage of salary you want to spend on rent: "))
+    electricity_percent = float(input("Enter the percentage of salary you want to spend on electricity: "))
     
