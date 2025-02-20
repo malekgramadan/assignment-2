82e8faefcda3b8fbfa9f0bd5bebca20c40658af7
Malek <mintywicked@gmail.com>
2025-02-20 10:39:35 +0200

    Adding the calculations for the savings, rent, electricity and the total

--- Nabiha's Salary.py
+++ Nabiha's Salary.py
@@ -7,4 +7,10 @@ while action != "no":
     savings_percent = float(input("Enter the percentage of salary you want to save: "))
     rent_percent = float(input("Enter the percentage of salary you want to spend on rent: "))
     electricity_percent = float(input("Enter the percentage of salary you want to spend on electricity: "))
-    
+        
+    savings = (savings_percent / 100) * salary
+    rent = (rent_percent / 100) * salary
+    electricity = (electricity_percent / 100) * salary
+    total = savings + rent + electricity
+        
+    list.append([month, salary, savings, rent, electricity, total])
