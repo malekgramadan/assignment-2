c536f9f93febb385496d3b60c24f2c68b92ddb52
Malek <mintywicked@gmail.com>
2025-02-21 13:35:43 +0200

    Adding a for loop for the output to display each month seperatly

--- Nabiha's Salary.py
+++ Nabiha's Salary.py
@@ -17,19 +17,19 @@ while action.lower() != "no": #Making sure that the user can add as many months 
     yearly_cost = (rent + electricity) * 12 #What the user spends in a year for electricity and rent
     doubled_salary = salary * 2 #the fun part
     
-    if add_savings.lower() == "yes": #If the user has additional savings
+    if add_savings.lower() != "yes":
         additional_savings = float(input("Enter the amount of additional savings: "))
         savings += additional_savings
         remaining -= additional_savings
 
     list.append([month, salary, savings, rent, electricity, total, remaining, yearly_cost, doubled_salary, additional_savings])#Adding the user's data to a list
     action = input("Do you want to add another month? (yes/no): ")
-print(f"You have {savings} in savings.")
-print(f"You have spent {rent} on rent.")
-print(f"You have spent {electricity} on electricity.")
-print(f"You have spent {total} in total.")
-print(f"You have {remaining} remaining.")
-print(f"You have spent {yearly_cost} in total for rent and electricity.")
-print(f"Your salary doubled is {doubled_salary}.")
-print(f"You added {additional_savings} to your savings.")
-print(list)
+
+for i in list: #Printing the user's data in a table
+    print(f"{i[0]}'s salary is {i[1]}, you have {i[2]} savings.")
+    print(f"You have spent {i[3]} on rent, and {i[4]} on electricity, for the total of {i[5]} with the savings.")
+    print(f"You have {i[6]} remaining.")
+    print(f"You would spend {i[7]} in total for rent and electricity.")
+    print(f"If your salary was doubled it would be {i[8]}.")
+    print(f"You have {i[9]} additional savings.")
+    print(" ")
